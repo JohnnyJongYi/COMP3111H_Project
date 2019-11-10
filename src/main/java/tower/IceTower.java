4d4db33669145e54035d42bb85fee4044da32eda
Yeoh Choon Hong <choonhong1998@gmail.com>
2019-11-10 21:58:23 +0800

    Tower

---
+++ src/main/java/tower/IceTower.java
@@ -0,0 +1,25 @@
+package tower;
+
+public class IceTower extends Tower{
+	
+	IceTower(int type, int ID, int x, int y)
+	{
+		super(type, ID, x, y);
+		printTowerInfo();
+	}
+	
+	public void shoot()
+	{
+		Monster* monster = null;
+		int min_distance = 480 * 480;
+		for(int i = 0; i < monsterCount; i++)
+		{
+			if monsterArray[i].distance < min_distance
+			{
+				monster = &(monsterArray[i]);
+				min_distance = 	monsterArray[i].distance
+			}
+		}
+		monster.damage(IceTower, 0)
+	}
+}
